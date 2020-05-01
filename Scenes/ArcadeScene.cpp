af34d43fb0aa28b7474fffbcc8948b58b3c45336
halex623 <halex623@gmail.com>
2020-05-01 19:22:52 -0400

    added arcade scene

---
+++ Scenes/ArcadeScene.cpp
@@ -0,0 +1,99 @@
+//
+// Created by Hrithvik  Alex on 2020-04-07.
+//
+
+#include "ArcadeScene.h"
+#include "../graphics/color.h"
+#include "../graphics/Screen.h"
+#include "../shapes/Rectangle.h"
+#include "../Input/GameController.h"
+#include <iostream>
+#include "../App/App.h"
+#include "GameScene.h"
+#include "NotImplementedScene.h"
+#include "../Games/BreakOut/BreakOut.h"
+#include "../Games/Tetris/Tetris.h"
+#include "../Games/Asteroids/Asteroids.h"
+
+ArcadeScene::ArcadeScene():ButtonOptionsScene({"Tetris", "Break Out!", "Asteroids", "!Pac-man"}, Color::Cyan()) {}
+
+ void ArcadeScene::init() {
+    std::vector<Button::ButtonAction> actions;
+
+    actions.push_back([this]{
+        App::Singleton().pushScene(getScene(TETRIS));
+    });
+
+     actions.push_back([this]{
+         App::Singleton().pushScene(getScene(BREAK_OUT));
+     });
+
+     actions.push_back([this]{
+         App::Singleton().pushScene(getScene(ASTEROIDS));
+     });
+
+     actions.push_back([this]{
+         App::Singleton().pushScene(getScene(PACMAN));
+     });
+
+     setButtonActions(actions);
+
+     ButtonOptionsScene::init();
+
+     {
+         mSpriteSheet.load("PacmanSprites");
+         mSprite.init(App::Singleton().getBasePath() + "../Assets/Pacman_animations.txt", mSpriteSheet);
+         mSprite.setAnimation("death", true);
+     }
+}
+
+ void ArcadeScene::update(uint32_t dt) {
+    mSprite.update(dt);
+}
+
+ void ArcadeScene::draw(Screen& theScreen) {
+    ButtonOptionsScene::draw(theScreen);
+
+    mSprite.draw(theScreen);
+}
+
+ const std::string& ArcadeScene::getSceneName() const {
+    static std::string sceneName = "Arcade";
+    return sceneName;
+}
+
+
+std::unique_ptr<Scene> ArcadeScene::getScene(eGame game) {
+    switch(game) {
+        case TETRIS: {
+            std::unique_ptr<Tetris> tetrisGame = std::make_unique<Tetris>();
+
+            std::unique_ptr<GameScene> tetrisScene = std::make_unique<GameScene>(std::move(tetrisGame));
+
+            return tetrisScene;
+        }
+
+        case BREAK_OUT: {
+            std::unique_ptr<BreakOut> breakoutGame = std::make_unique<BreakOut>();
+
+            std::unique_ptr<GameScene> breakoutScene = std::make_unique<GameScene>(std::move(breakoutGame));
+
+            return breakoutScene;
+        }
+
+        case ASTEROIDS: {
+            std::unique_ptr<Asteroids> asteroidsGame = std::make_unique<Asteroids>();
+
+            std::unique_ptr<GameScene> asteroidsScene = std::make_unique<GameScene>(std::move(asteroidsGame));
+
+            return asteroidsScene;
+        }
+
+        case PACMAN: {
+
+        }
+    }
+
+    std::unique_ptr<Scene> notImplementedScene = std::make_unique<NotImplementedScene>();
+    return notImplementedScene;
+}
---
+++ Scenes/ArcadeScene.h
@@ -0,0 +1,36 @@
+//
+// Created by Hrithvik  Alex on 2020-04-07.
+//
+
+#ifndef LINES_ARCADESCENE_H
+#define LINES_ARCADESCENE_H
+
+#include "ButtonOptionsScene.h"
+#include "../graphics/AnimatedSprite.h"
+#include <memory>
+
+enum eGame {
+    TETRIS = 0,
+    BREAK_OUT,
+    ASTEROIDS,
+    PACMAN,
+    NUM_GAMES,
+};
+
+class ArcadeScene: public ButtonOptionsScene {
+public:
+    ArcadeScene();
+    virtual void init() override;
+    virtual void update(uint32_t dt) override;
+    virtual void draw(Screen& theScreen) override;
+    virtual const std::string& getSceneName() const override;
+
+private:
+    std::unique_ptr<Scene> getScene(eGame game);
+
+    AnimatedSprite mSprite;
+    SpriteSheet mSpriteSheet;
+};
+
+
+#endif //LINES_ARCADESCENE_H
