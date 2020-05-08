5a97147e981063f0ce11814ef79c70a60e4d302f
halex623 <halex623@gmail.com>
2020-05-08 19:17:08 -0400

    added pacman utils

---
+++ Games/Pacman/PacmanGameUtils.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Hrithvik  Alex on 2020-05-08.
+//
+
+#include "PacmanGameUtils.h"
+
+Vec2D getMovementVector(PacmanMovement direction) {
+    if(direction == PACMAN_MOVEMENT_LEFT) {
+        return Vec2D(-1,0);
+    } else if(direction == PACMAN_MOVEMENT_DOWN) {
+        return Vec2D(0,1);
+    } else if(direction == PACMAN_MOVEMENT_RIGHT) {
+        return Vec2D(1,0);
+    } else if(direction == PACMAN_MOVEMENT_UP) {
+        return Vec2D(0, -1);
+    }
+
+    return Vec2D::Zero;
+}
+
+PacmanMovement getOppositeDirection(PacmanMovement direction) {
+    if(direction == PACMAN_MOVEMENT_LEFT) {
+        return PACMAN_MOVEMENT_RIGHT;
+    } else if(direction == PACMAN_MOVEMENT_DOWN) {
+        return PACMAN_MOVEMENT_UP;
+    } else if(direction == PACMAN_MOVEMENT_RIGHT) {
+        return PACMAN_MOVEMENT_LEFT;
+    } else if(direction == PACMAN_MOVEMENT_UP) {
+        return PACMAN_MOVEMENT_DOWN;
+    }
+
+    return PACMAN_MOVEMENT_NONE;
+}
+
+std::vector<PacmanMovement>  getPerpendicularMovements(PacmanMovement direction) {
+
+    std::vector<PacmanMovement> perpendicularDirections;
+
+    if(direction == PACMAN_MOVEMENT_LEFT || direction == PACMAN_MOVEMENT_RIGHT) {
+        perpendicularDirections.push_back(PACMAN_MOVEMENT_UP);
+        perpendicularDirections.push_back(PACMAN_MOVEMENT_DOWN);
+    } else if(direction == PACMAN_MOVEMENT_DOWN || direction == PACMAN_MOVEMENT_UP) {
+        perpendicularDirections.push_back(PACMAN_MOVEMENT_LEFT);
+        perpendicularDirections.push_back(PACMAN_MOVEMENT_RIGHT);
+    } else if(direction == PACMAN_MOVEMENT_NONE) {
+        perpendicularDirections.push_back(PACMAN_MOVEMENT_UP);
+        perpendicularDirections.push_back(PACMAN_MOVEMENT_LEFT);
+        perpendicularDirections.push_back(PACMAN_MOVEMENT_DOWN);
+        perpendicularDirections.push_back(PACMAN_MOVEMENT_RIGHT);
+    }
+
+    return perpendicularDirections;
+}
---
+++ Games/Pacman/PacmanGameUtils.h
@@ -0,0 +1,27 @@
+//
+// Created by Hrithvik  Alex on 2020-05-08.
+//
+
+#ifndef LINES_PACMANGAMEUTILS_H
+#define LINES_PACMANGAMEUTILS_H
+
+#include "../../Utils/vec2D.h"
+#include <vector>
+
+enum PacmanMovement {
+    PACMAN_MOVEMENT_NONE = 0,
+    PACMAN_MOVEMENT_UP,
+    PACMAN_MOVEMENT_LEFT,
+    PACMAN_MOVEMENT_DOWN,
+    PACMAN_MOVEMENT_RIGHT,
+};
+
+Vec2D getMovementVector(PacmanMovement direction);
+
+PacmanMovement getOppositeDirection(PacmanMovement direction);
+
+std::vector<PacmanMovement>  getPerpendicularMovements(PacmanMovement direction);
+
+
+
+#endif //LINES_PACMANGAMEUTILS_H
